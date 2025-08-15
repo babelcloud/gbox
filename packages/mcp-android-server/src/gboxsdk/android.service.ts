151d8fb4b5987e3d5ec9441b39a90e2295b8413a
Hailong Zhang <zhlmmc@gmail.com>
2025-08-15 09:48:51 -0700

    Hailong/start box support physical devices (#200)
    * start box support physical devices
    
    * update readme

--- packages/mcp-android-server/src/gboxsdk/android.service.ts
+++ packages/mcp-android-server/src/gboxsdk/android.service.ts
@@ -1,5 +1,6 @@
 import GboxSDK, { AndroidBoxOperator } from "gbox-sdk";
 import { config } from "../config.js";
+import axios from "axios";
 
 // Initialize Gbox SDK
 const gboxSDK = new GboxSDK({ apiKey: config.gboxApiKey });
@@ -12,6 +13,47 @@ export async function attachBox(boxId: string): Promise<AndroidBoxOperator> {
     throw new Error(
       `Failed to attach to box ${boxId}: ${(err as Error).message}`
     );
+  }
+}
+
+export type AndroidDevice = {
+  id: string;
+  model: string;
+  status: "online" | "offline";
+  enabled: boolean;
+  isIdle: boolean;
+}
+
+export async function deviceList(availableOnly: boolean = true): Promise<AndroidDevice[]> {
+  // TODO: use gbox-sdk to get device list and should be able to change baseUrl
+  const apiUrl = `https://gbox.ai/api/dashboard/v1/device/device_list`;
+  const response = await axios.post(
+    apiUrl,
+    {},
+    {
+      headers: {
+        "x-api-key": config.gboxApiKey
+      }
+    }
+  );
+
+  try {
+    const devices = response.data.devices.map((device: any) => ({
+      id: device.deviceId,
+      model: device.deviceData["ro.product.model"],
+      status: device.status,
+      enabled: device.enable,
+      isIdle: device.isIdle
+    }));
+
+    if (availableOnly) {
+      return devices.filter((device: AndroidDevice) => device.status === "online" && device.enabled && device.isIdle);
+    }
+
+    return devices;
+  } catch (err) {
+    console.error(err);
+    return [];
   }
 }
 
