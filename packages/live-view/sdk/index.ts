f2507de7d68adbc347b0ed59c051480083a78fc0
zhangze <1183460943@qq.com>
2025-10-10 11:14:37 +0800

    feat: add publish npm

--- packages/live-view/sdk/index.ts
+++ packages/live-view/sdk/index.ts
@@ -1,10 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { AndroidLiveviewComponent, AndroidLiveviewComponentProps } from "./AndroidLiveviewComponent";
-
-
-export function createAndroidLiveView(mount: HTMLElement, props: AndroidLiveviewComponentProps) {
-  const root = (ReactDOM as any).createRoot(mount);
-  root.render(React.createElement(AndroidLiveviewComponent, props));
-  return root;
-}
+export { AndroidLiveviewComponent } from "./AndroidLiveviewComponent";
+export type { AndroidLiveviewComponentProps } from "./AndroidLiveviewComponent";
