09081fb52ba1e5e890f981d94d89694e2631a5b6
zhangze <1183460943@qq.com>
2025-10-10 10:27:57 +0800

    feat: add publish npm

--- packages/live-view/vite.config.sdk.ts
+++ packages/live-view/vite.config.sdk.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dts from "vite-plugin-dts";
+// import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    dts({
-      insertTypesEntry: true,
-      include: ['sdk/**/*'],
-      exclude: ['**/*.test.*', '**/*.spec.*']
-    })
+    // dts({
+    //   insertTypesEntry: true,
+    //   include: ['sdk/**/*'],
+    //   exclude: ['**/*.test.*', '**/*.spec.*']
+    // })
   ],
   build: {
     outDir: "dist",
